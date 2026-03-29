4e5e20c8c469540f27341fa8bf87d951a7cecd0a
alinweshi <alinweshi@gmail.com>
2026-03-29 02:33:04 +0200

    initiate student model

---
+++ database/factories/StudentFactory.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\student;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends Factory<student>
+ */
+class StudentFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            //
+        ];
+    }
+}
